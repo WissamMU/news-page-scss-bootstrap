04deb3113eb95b20d049acf338fa6c46f961560d
WissamMU <wessam.2003.ali@gmail.com>
2024-10-06 22:55:57 +0300

    videos sections

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
